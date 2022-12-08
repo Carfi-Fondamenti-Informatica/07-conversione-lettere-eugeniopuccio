d705862ed197b809c9eb8431e6af9b8eec82b714
eugeniopuccio <114490589+eugeniopuccio@users.noreply.github.com>
2022-12-08 23:22:52 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
-
+#include "lib.h"
+bool checkimput(int x){
+    if((x>=65 && x<=90)||(x>=97 && x<=122))
+        return true;
+    else
+        return false;
+}
+char conversione(char x){
+    char ris;
+        if (x >= 'A' && x <= 'Z')
+            ris= x+32;
+        else if (x>= 'a' && x<= 'z')
+            ris= x-32;
+        return ris;
+}
