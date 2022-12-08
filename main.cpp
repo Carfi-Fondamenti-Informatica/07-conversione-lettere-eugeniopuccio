46449857aaabced1f204ad33f6759a8cdf85d596
eugeniopuccio <114490589+eugeniopuccio@users.noreply.github.com>
2022-12-08 23:20:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char c;
+    char v;
+    cin>>c;
+    if (checkimput(c)){
+        v= conversione(c);
+        cout<<v;
+    }else {
+        cout<<"errore"<<endl;
+    }
+    return 0;
 }
