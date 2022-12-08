9b6cbbfba15c373bc53b453f105f94b4932b4152
eugeniopuccio <114490589+eugeniopuccio@users.noreply.github.com>
2022-12-08 23:22:10 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,9 @@
+//
+// Created by Eugenio Puccio on 08/12/22.
+//
 
+#ifndef UNTITLED13_LIB_H
+#define UNTITLED13_LIB_H
+char conversione(char x);
+bool checkimput(int x);
+#endif //UNTITLED13_LIB_H
